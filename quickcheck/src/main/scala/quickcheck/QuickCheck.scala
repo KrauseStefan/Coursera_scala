de92a4ed04ba44435f81dd06d67071f585d4094f
unknown <skk@pc-4187.systematicgroup.local>
2015-04-20 12:48:57 +0200

    L1: working solution

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -19,40 +19,13 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   def isSorted_rec(min: A, h: H): Boolean = {
     if(isEmpty(h)) {
-//      println("Success")
       return true
     }
     if(min > findMin(h)){
-//      println("failed " + min + " > " + findMin(h))
       return false
     }
-//    print(findMin(h) + ", ")
 
     isSorted_rec(findMin(h), deleteMin(h))
-  }
-
-  def isSorted_max(h: H, max: Int): Boolean = {
-    if(isEmpty(h))
-      true
-    else
-      isSorted_rec_max(findMin(h), deleteMin(h), max - 1)
-  }
-
-  def isSorted_rec_max(min: A, h: H, max: Int): Boolean = {
-    if(isEmpty(h)) {
-      //      println("Success")
-      if(max == 0)
-        return true
-      else
-        return false
-    }
-    if(min > findMin(h)){
-      //      println("failed " + min + " > " + findMin(h))
-      return false
-    }
-    //    print(findMin(h) + ", ")
-
-    isSorted_rec_max(findMin(h), deleteMin(h), max -1)
   }
 
   property("min1") = forAll { a: Int =>
@@ -60,16 +33,10 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
-  property("min2") = forAll { (a: Int, b: Int) =>
-    val h = insert(a, empty)
-    val j = insert(b, h)
-    findMin(j) == (if(a > b) b else a)
-  }
-
   property("min3") = forAll { (a: Int, b: Int, c: Int) =>
-    var h = insert(a, empty)
-    h = insert(b, h)
-//    h = insert(c, h)
+    var h = insert(a, insert(b, insert(c, empty)))
+
+    val listA = Vector(a, b, c).sorted
 
     val x = findMin(h)
     h = deleteMin(h)
@@ -77,32 +44,12 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     val y = findMin(h)
     h = deleteMin(h)
 
-
-/*
     val z = findMin(h)
     h = deleteMin(h)
-*/
 
-    if(isEmpty(h)){
-      if(x == y && a == b)
-        true
-      else if(a > b && b == x && a == y)
-        true
-      else if(a < b && b == y && a == x)
-        true
-      else
-        false
-    }else{
-      false
-    }
-  }
+    val listB = Vector(x,y,z)
 
-  property("sorted_fixed1") = forAll { (a: Int, b: Int, c: Int) =>
-    var h = insert(a, empty)
-    h = insert(b, h)
-    h = insert(c, h)
-
-    isSorted_max(h, 3)
+    listA.sameElements(listB)
   }
 
   property("empty") = forAll { a: Int =>
@@ -124,15 +71,9 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     isSorted(h)
   }
 
-  property("sorted3") = forAll { (h1: H, h2: H, h3: H) =>
-    var h = meld(h1, h2)
-    h = meld(h, h3)
-    isSorted(h)
-  }
-
   property("gen1") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
-    findMin(insert(m, h))==m
+    findMin(insert(m, h)) == m
   }
 
   lazy val genHeap: Gen[H] = for {
