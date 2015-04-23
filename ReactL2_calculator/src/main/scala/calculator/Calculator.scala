4886bc81b0f552b7d6db56dd6c5352b3b5fca963
stkkj <stefan.krausekjaer@gmail.com>
2015-04-23 22:01:37 +0200

    L2: calc almost done, need error handling

--- ReactL2_calculator/src/main/scala/calculator/Calculator.scala
+++ ReactL2_calculator/src/main/scala/calculator/Calculator.scala
@@ -9,13 +9,38 @@ final case class Times(a: Expr, b: Expr) extends Expr
 final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
-  def computeValues(
-      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+  def computeValues(namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
+    var result: Map[String, Signal[Double]] = Map.empty
+
+    for (
+      (key, expr) <- namedExpressions
+    ) yield key -> Signal[Double] {
+      eval(expr(), namedExpressions)
+    }
+    /*    {
+          result = result + (key -> Signal[Double]{
+            eval(expr(), namedExpressions)
+          })
+        }
+        result*/
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    expr match {
+      case Literal(l) =>
+        l
+      case Ref(r) =>
+        eval(references(r)(), references)
+      case Plus(a, b) =>
+        eval(a, references) + eval(b, references)
+      case Minus(a, b) =>
+        eval(a, references) - eval(b, references)
+      case Times(a, b) =>
+        eval(a, references) * eval(b, references)
+      case Divide(a, b) =>
+        eval(a, references) / eval(b, references)
+
+    }
   }
 
   /** Get the Expr for a referenced variables.
